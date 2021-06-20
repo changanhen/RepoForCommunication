fdfc77e50b774f243bba68fd9b63a0feed73a93f
abdtyx <1507844577@qq.com>
2021-06-20 12:56:02 +0800

    edit1

--- tyx/cpp/LinearList/main.cpp
+++ tyx/cpp/LinearList/main.cpp
@@ -9,39 +9,39 @@ typedef int DataType;
 //定义线性表的结构
 typedef struct LIST
 {
-	DataType* list;    //指向线性表的指针
-	int length;   //表长
-	int maxLength;     //表容量
-}ListType;
+	DataType *list; //指向线性表的指针
+	int length;		//表长
+	int maxLength;	//表容量
+} ListType;
 
 //声明线性表具有的方法
-ListType* CreateList(int length);     //创建一个长度为length的线性表
-void DestroyList(ListType* pList);     //销毁线性表
-void ClearList(ListType* pList);      //置空线性表
-int IsEmptyList(ListType* pList);     //检测线性表是否为空
-int GetListLength(ListType* pList);   //获取线性表长度
-int GetListElement(ListType* pList, 
-	int n, DataType* data);   //获取线性表中第n个元素
-int FindElement(ListType* pList, 
-	int pos, DataType data);      //从pos起查找data 第一次出现的位置
-int GetPriorElement(ListType* pList, 
-	int n, DataType* data);   //获取第n个元素的前驱
-int GetNextElement(ListType* pList, int n, DataType* data);     //获取第n个元素的后继
-int InsertToList(ListType* pList, int pos, DataType data);  //将 data插入到 pos 处
-int DeleteFromList(ListType* pList, int pos);             //删除线性表上位置为pos 的元素
-void PrintList(ListType* pList);          //输出线性表
+ListType *CreateList(int length);	//创建一个长度为length的线性表
+void DestroyList(ListType *pList);	//销毁线性表
+void ClearList(ListType *pList);	//置空线性表
+int IsEmptyList(ListType *pList);	//检测线性表是否为空
+int GetListLength(ListType *pList); //获取线性表长度
+int GetListElement(ListType *pList,
+				   int n, DataType *data); //获取线性表中第n个元素
+int FindElement(ListType *pList,
+				int pos, DataType data); //从pos起查找data 第一次出现的位置
+int GetPriorElement(ListType *pList,
+					int n, DataType *data);					//获取第n个元素的前驱
+int GetNextElement(ListType *pList, int n, DataType *data); //获取第n个元素的后继
+int InsertToList(ListType *pList, int pos, DataType data);	//将 data插入到 pos 处
+int DeleteFromList(ListType *pList, int pos);				//删除线性表上位置为pos 的元素
+void PrintList(ListType *pList);							//输出线性表
 
 /* 主函数，创建一个线性表，并测试*/
 int main()
 {
-	const int MAXLENGTH = 1000;   //假设最大容量为1000
+	const int MAXLENGTH = 1000; //假设最大容量为1000
 	//创建线性表
-	ListType* sqList = CreateList(MAXLENGTH);
+	ListType *sqList = CreateList(MAXLENGTH);
 	//以下是对线形表的测试
 	sqList->list[0] = 9;
 	sqList->length++;
-	
-	ClearList(sqList);         //置表为空
+
+	ClearList(sqList); //置表为空
 	//插入10个元素并显示
 	for (int i = 0; i < 10; ++i)
 		InsertToList(sqList, i, i + 1);
@@ -59,9 +59,9 @@ int main()
 	DataType data;
 	if (GetPriorElement(sqList, 3, &data) > -1)
 		printf("第3个元素的前驱是%d\n", data);
-	
+
 	DataType x;
-	int success=GetListElement(sqlist, 2, &x);
+	int success = GetListElement(sqlist, 2, &x);
 	return 0;
 }
 //线性表方法实现
@@ -71,15 +71,42 @@ int main()
  *@return 成功返回直线该表的指针，否则返回NULL
 */
 
+ListType *CreateList(int length)
+{
+	ListType *splist = (ListType *)malloc(sizeof(ListType));
+	if (splist != NULL)
+	{
+		splist->list = (DataType *)malloc(sizeof(DataType) * length);
+		if (splist->list == NULL)
+			return NULL;
+		splist->length = 0;
+		splist->maxLength = length;
+	}
+	return splist;
+}
+
 /**
  *@brief 销毁线性表
  *@param pList 指向需要销毁的线性表的指针
 */
 
+void DestroyList(ListType* plist)
+{
+	free(plist);
+}
+
 /**
 *@brief 置空线性表
 *@param pList 指向需要置空线性表的指针
 */
+
+void ClearList(ListType* plist)
+{
+	for (int i = 0; i < plist->maxLength; i++)
+	{
+		plist->list[i] = 0;
+	}
+}
 
 /**
 *@brief 检测线性表是否为空
@@ -144,4 +171,3 @@ int main()
 *@brief 输出线性表
 *@param pList 指向线性表的指针
 */
-
