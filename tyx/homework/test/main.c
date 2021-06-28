ce42cb216c68d4d76b987deed72140f972eb48d9
abdtyx <1507844577@qq.com>
2021-06-28 22:01:30 +0800

    updated

--- tyx/homework/test/main.c
+++ tyx/homework/test/main.c
@@ -1,9 +1,28 @@
+
+/*
 #include <stdio.h>
-typedef bool __crt_bool;
 int main()
 {
-    __crt_bool bl;
-    int a = 1, b = 2;
-    bl = (a == b);
-    printf("%d", bl);
+    char ch;
+    scanf("%c", &ch);
+    printf("%c%c%c", (ch - 1 + 26 - 'a') % 26 + 'a', ch, (ch + 1 + 26 - 'a') % 26 + 'a');
 }
+*/
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    char kstr[100];
+    int num, k;
+    scanf("%d%d", &num, &k);
+    itoa(num, kstr, k);
+    _strupr(kstr);
+    printf("%s", kstr);
+}
+int Josephus(int n, int m)
+{
+    return n == 1 ? n : (Josephus(n - 1, m) + m - 1) % n + 1;
+}
+*/
